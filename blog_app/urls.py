673a3e707e1880c60154772279f0a0dc9116c0ce
unknown <amir98sajadian@gmail.com>
2025-10-07 23:26:13 +0330

    seart_1

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,4 +4,5 @@ from .import views
 app_name = 'blog'
 urlpatterns = [path('detail/<slug:slug>',views.post_detail,name='article_detail'),
                path('all artikle',views.allarticle_list,name='articles_list'),
-               path('categori/<int:pk>',views.category_detail,name='categor_detail')]
+               path('categori/<int:pk>',views.category_detail,name='categor_detail'),
+               path('search/',views.search,name='search_articles')]
--- blog_app/views.py
+++ blog_app/views.py
@@ -17,12 +17,18 @@ def allarticle_list(request):
     article = artikle.objects.all()
     footer1 = footer.objects.last()
     page_number = request.GET.get('page')
-    paginatori = Paginator(article,2)
+    paginatori = Paginator(article,1)
     object_list = paginatori.get_page(page_number)
     return render(request,'blog_app/all_artikle_list.html',{'articli':object_list , 'footers' : footer1})
 def category_detail(request,pk=None):
     categor=get_object_or_404(category,id=pk)
     arti=categor.artikle_set.all()
     return render(request,'blog_app/all_artikle_list.html',{'articli':arti})
-
+def search(request):
+    q = request.GET.get('q')
+    articl = artikle.objects.filter(slug__icontains = q)
+    page_number = request.GET.get('page')
+    pagin = Paginator(articl,1)
+    object_list=pagin.get_page(page_number)
+    return render(request,'blog_app/all_artikle_list.html',{'articli':object_list})
 # Create your views here.
