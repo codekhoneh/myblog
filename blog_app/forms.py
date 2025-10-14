dd87c3f1041320dcc7a5f1c7cd556a15176f85c5
myblogfa <codekhoneh@gmail.com>
2025-10-14 11:38:42 +0330

    dvfd

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from .models import ContactMessage
+from django.core.validators import ValidationError
 
 class ContactUsForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
         fields = ['name', 'text']
+ 
+    def clean(self):
+        name=self.cleaned_data.get('name')
+        text=self.cleaned_data.get('text')
+       
+        if name==text:
+            raise ValidationError('خطا:نام و متن نظرات نمی تواند یکسان باشد',code='name_text_same')
+    def clean_name(self):
+        name=self.cleaned_data.get('name')
+        if '@' or '*'  in name:
+             raise ValidationError('@ یا* نمی تواند در نام استفاده شود',code='@_in code')
+        return name
+    
+    
